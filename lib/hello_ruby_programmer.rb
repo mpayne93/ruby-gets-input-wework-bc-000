04262757a19dd70ecdfa773f39156e6b0c78e938
Maurice Payne <maurice.payne@wework.com>
2018-02-13 23:22:01 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -2,5 +2,5 @@ def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 
-puts "Please enter your name so that we can greet you more personally"
+puts "Please enter your name so that we can greet you more personally:"
 input = gets
